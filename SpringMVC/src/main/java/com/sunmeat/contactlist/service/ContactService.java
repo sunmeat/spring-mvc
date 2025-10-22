d19f08dee5f73fe13a4698d370d983c7d8035322
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-10-22 14:48:00 +0300

    Remove unnecessary whitespace and format ContactService

--- SpringMVC/src/main/java/com/sunmeat/contactlist/service/ContactService.java
+++ SpringMVC/src/main/java/com/sunmeat/contactlist/service/ContactService.java
@@ -1,32 +1,31 @@
 package com.sunmeat.contactlist.service;
 
-import com.sunmeat.contactlist.model.Contact; 
+import com.sunmeat.contactlist.model.Contact;
 import com.sunmeat.contactlist.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ContactService {
 
-    @Autowired
-    private ContactRepository contactRepository;
+	@Autowired
+	private ContactRepository contactRepository;
 
-    public List<Contact> getAllContacts() {
-        return contactRepository.findAll();
-    }
+	public List<Contact> getAllContacts() {
+		return contactRepository.findAll();
+	}
 
-    public Optional<Contact> getContactById(Long id) {
-        return contactRepository.findById(id);
-    }
+	public Optional<Contact> getContactById(Long id) {
+		return contactRepository.findById(id);
+	}
 
-    public Contact saveContact(Contact contact) {
-        return contactRepository.save(contact);
-    }
+	public Contact saveContact(Contact contact) {
+		return contactRepository.save(contact);
+	}
 
-    public void deleteContact(Long id) {
-        contactRepository.deleteById(id);
-    }
+	public void deleteContact(Long id) {
+		contactRepository.deleteById(id);
+	}
 }
